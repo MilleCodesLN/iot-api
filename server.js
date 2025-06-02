e06f3bd3cf1d0be80a3be0159b65e0c849162a6a
MilleCodesLN <48634455+MilleCodesLN@users.noreply.github.com>
2025-06-02 16:08:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Middleware pour parser les JSON
 app.use(express.json());
